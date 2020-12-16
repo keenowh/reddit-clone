bc5d6e9eca8406250ae7854abe034f7f3d0e9be4
Keen Ballon <keenballon@gmail.com>
2020-12-16 23:20:32 +0800

    added page for single post

---
+++ web/src/pages/post/[id].tsx
@@ -0,0 +1,23 @@
+import { withUrqlClient } from "next-urql";
+import { useRouter } from "next/router";
+import React from "react";
+import { createUrqlClient } from "../../../utils/createUrqlClient";
+
+export const Post = ({}) => {
+  const router = useRouter();
+  const intId =
+    typeof router.query.id === "string" ? parseInt(router.query.id) : -1;
+
+  const [{ data, fetching }] = usePostQuery({
+    pause: intId === -1,
+    variables: {
+      id: intId,
+    },
+  });
+
+  router.query.id;
+
+  return <Layout></Layout>;
+};
+
+export default withUrqlClient(createUrqlClient, { ssr: true })(Post);
